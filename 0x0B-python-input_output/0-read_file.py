c1af72c4627f459b4aff36937983b014bba5af9c
Oladotun25 <osadotunsamuel25@gmail.com>
2022-10-25 15:06:29 -0700

    task 0 done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    """ function that reads a text file (UTF8) and prints it to stdout """
+'''
+Write a function that reads a text file (UTF8)
+and prints it to stdout
+'''
 
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
+
+def read_file(filename=""):
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            print(line, end='')
+    f.close()
